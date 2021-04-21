9ada7f2a633e8e2376844486ccd6b4c68afe8d20
camosss <camosss@naver.com>
2021-04-21 23:14:09 +0900

    Get User Data From Firestore & Create Data Model

---
+++ InstagramFireStoreTutorial/API/UserService.swift
@@ -0,0 +1,19 @@
+//
+//  UserService.swift
+//  InstagramFireStoreTutorial
+//
+//  Created by 강호성 on 2021/04/21.
+//
+
+import Firebase
+
+struct UserService {
+    static func fetchUser(completion: @escaping(User) -> Void) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        COLLECTION_USERS.document(uid).getDocument { snapshot, error in
+            guard let dictionary = snapshot?.data() else { return }
+            let user = User(dictionary: dictionary)
+            completion(user)
+        }
+    }
+}
---
+++ InstagramFireStoreTutorial/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  InstagramFireStoreTutorial
+//
+//  Created by 강호성 on 2021/04/21.
+//
+
+import Firebase
+
+let COLLECTION_USERS = Firestore.firestore().collection("users")
