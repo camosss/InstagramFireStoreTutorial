fc9c2d76b68857c21f3721afe581b746e07caeab
camosss <camosss@naver.com>
2021-04-28 01:35:18 +0900

    Fetch Comments From API

--- InstagramFireStoreTutorial/API/CommentService.swift
+++ InstagramFireStoreTutorial/API/CommentService.swift
@@ -22,7 +22,26 @@ struct CommentService {
                                                                              completion: completion)
     }
     
-    static func fetchComments() {
+    static func fetchComments(forPost postID: String, completion: @escaping([Comment]) -> Void) {
         
+        var comments = [Comment]()
+        
+        let query = COLLECTION_POSTS.document(postID).collection("comments")
+            .order(by: "timestamp", descending: true)
+        
+        // - addSnapshotListener
+        // 해당 comment를 수신한 다음 해당 comment를 기반으로 UI를 업데이트
+        // API 호출을 수동으로 수행할 필요없이 데이터베이스에 추가되는 즉시
+        // 새로운 comment가 데이터를 새로고침하기 위해 포스트하는 것과 같음
+        query.addSnapshotListener { (snapshot, error) in  // snapshot을 보고
+            snapshot?.documentChanges.forEach({ change in // 문서 변경사항(documentChanges)을 보고
+                if change.type == .added {                // forEach문으로 반복해서 변경 유형이 추가되었는지 확인
+                    let data = change.document.data()
+                    let comment = Comment(dictionary: data)
+                    comments.append(comment)
+                }
+            })
+            completion(comments)
+        }
     }
 }
---
+++ InstagramFireStoreTutorial/Model/Comment.swift
@@ -0,0 +1,24 @@
+//
+//  Comment.swift
+//  InstagramFireStoreTutorial
+//
+//  Created by 강호성 on 2021/04/28.
+//
+
+import Firebase
+
+struct Comment {
+    var uid: String
+    let username: String
+    let profileImageUrl: String
+    let timestamp: Timestamp
+    let commentText: String
+    
+    init(dictionary: [String: Any]) {
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+        self.commentText = dictionary["commentText"] as? String ?? ""
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+    }
+}
