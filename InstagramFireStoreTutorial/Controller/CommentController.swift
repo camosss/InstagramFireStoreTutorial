f750588d04a63eef18ab527fc07ead6b6318ac80
camosss <camosss@naver.com>
2021-04-28 12:32:55 +0900

    Go To User Profile From Comment Tap

--- InstagramFireStoreTutorial/Controller/CommentController.swift
+++ InstagramFireStoreTutorial/Controller/CommentController.swift
@@ -94,6 +94,19 @@ extension CommentController {
     }
 }
 
+    // MARK: - UICollectionViewDelegate
+
+extension CommentController {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let uid = comments[indexPath.row].uid
+        // print("DEBUG: Comment uid in \(uid)")
+        UserService.fetchUser(withUid: uid) { user in
+            let controller = ProfileController(user: user)
+            self.navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+}
+
     // MARK: - UICollectionViewDelegateFlowLayout
 
 extension CommentController: UICollectionViewDelegateFlowLayout {
