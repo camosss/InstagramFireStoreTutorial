c97c1bb3107f762f306fa2994303da6e5b362d51
camosss <camosss@naver.com>
2021-04-30 13:11:59 +0900

    Tap the profile image and username in the feed to go to the user's profile

--- InstagramFireStoreTutorial/View/FeedCell.swift
+++ InstagramFireStoreTutorial/View/FeedCell.swift
@@ -12,6 +12,7 @@ import UIKit
 protocol FeedCellDelegate: class {
     func cell(_ cell: FeedCell, wantsToShowCommentsFor post: Post)
     func cell(_ cell: FeedCell, didLike post: Post)
+    func cell(_ cell: FeedCell, wantsToShowPrifileFor uid: String)
 }
 
 class FeedCell: UICollectionViewCell {
@@ -24,12 +25,17 @@ class FeedCell: UICollectionViewCell {
     
     weak var delegate: FeedCellDelegate?
     
-    private let profileImageView: UIImageView = {
+    private lazy var profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
         iv.isUserInteractionEnabled = true
         iv.backgroundColor = .lightGray
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(showUserProfile))
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(tap)
+        
         return iv
     }() // () = 클로저를 실행, ()를 빼면 변수가 클로저 변수가 된다.
     
@@ -37,7 +43,7 @@ class FeedCell: UICollectionViewCell {
         let button = UIButton(type: .system)
         button.setTitleColor(.black, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
-        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        button.addTarget(self, action: #selector(showUserProfile), for: .touchUpInside)
         return button
     }()
     
@@ -137,8 +143,10 @@ class FeedCell: UICollectionViewCell {
     
     // MARK: - Actions
     
-    @objc func didTapUsername() {
-        print("DEBUG: did tap username")
+    @objc func showUserProfile() {
+//        print("DEBUG: did tap username")
+        guard let viewModel = viewModel else { return }
+        delegate?.cell(self, wantsToShowPrifileFor: viewModel.post.ownerUid)
     }
     
     @objc func didTapComments() {
@@ -155,6 +163,7 @@ class FeedCell: UICollectionViewCell {
     
     func configure() {
         guard let viewModel = viewModel else { return }
+        
         captionLabel.text = viewModel.caption
         // 피드에 내가 추가한 이미지 보이기
         postImageView.sd_setImage(with: viewModel.imageUrl)
