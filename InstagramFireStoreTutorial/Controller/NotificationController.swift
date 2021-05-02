e00c4d3c22ff52be0cdb367a167de785ea6a2dec
camosss <camosss@naver.com>
2021-05-02 19:39:41 +0900

    Notification Tap Handlers & Follow Button Configuration

--- InstagramFireStoreTutorial/Controller/NotificationController.swift
+++ InstagramFireStoreTutorial/Controller/NotificationController.swift
@@ -30,6 +30,19 @@ class NotificationController: UITableViewController {
     func fetchNotifications() {
         NotificationService.fetchNotification { notifications in
             self.notifications = notifications
+            self.checkIfUserIsFollowed()
+        }
+    }
+    
+    func checkIfUserIsFollowed() {
+        notifications.forEach { notification in
+            guard notification.type == .follow else { return }
+            
+            UserService.checkIfUserIsFollowed(uid: notification.uid) { isFollowed in
+                if let index = self.notifications.firstIndex(where: { $0.id == notification.id }) {
+                    self.notifications[index].userIsFollowed = isFollowed
+                }
+            }
         }
     }
     
@@ -37,13 +50,15 @@ class NotificationController: UITableViewController {
     
     func configureTableView() {
         view.backgroundColor = .white
-        navigationController?.title = "Notifications"
+        navigationItem.title = "Notifications"
         
         tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 80
         tableView.separatorStyle = .none
     }
 }
+
+    // MARK: - UITabelViewDataSource
 
 extension NotificationController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -53,6 +68,44 @@ extension NotificationController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
         cell.viewModel = NotificationViewModel(notification: notifications[indexPath.row])
+        cell.delegate = self
         return cell
     }
 }
+
+    // MARK: - UITableViewDelegate
+
+extension NotificationController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        UserService.fetchUser(withUid: notifications[indexPath.row].uid) { user in
+            let controller = ProfileController(user: user)
+            self.navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+}
+
+    // MARK: - NotificationCellDelegate
+
+extension NotificationController: NotificationCellDelegate {
+    func cell(_ cell: NotificationCell, wantsToFollow uid: String) {
+        UserService.follow(uid: uid) { _ in
+            cell.viewModel?.notification.userIsFollowed.toggle()
+        }
+    }
+    
+    func cell(_ cell: NotificationCell, wantsToUnFollow uid: String) {
+        UserService.unfollow(uid: uid) { _ in
+            cell.viewModel?.notification.userIsFollowed.toggle()
+        }
+    }
+    
+    func cell(_ cell: NotificationCell, wantsToViewPost postId: String) {
+        PostService.fetchPost(withPostId: postId) { post in
+            let controller = FeedController(collectionViewLayout: UICollectionViewFlowLayout())
+            controller.post = post
+            self.navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+}
+
+
