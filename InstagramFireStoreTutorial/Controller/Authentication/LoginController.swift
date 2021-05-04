eb96565ffaffa88d7ccd969cc939f5f5831a5061
camosss <camosss@naver.com>
2021-05-04 20:45:51 +0900

    Reset Password Controller UI

--- InstagramFireStoreTutorial/Controller/Authentication/LoginController.swift
+++ InstagramFireStoreTutorial/Controller/Authentication/LoginController.swift
@@ -52,6 +52,7 @@ class LoginController: UIViewController {
     private let forgotPasswordButton: UIButton = {
         let button = UIButton(type: .system)
         button.attributedTitle(firstPart: "Forgot password? ", secondPart: "Get help siging in.")
+        button.addTarget(self, action: #selector(handleShowResetPassword), for: .touchUpInside)
         return button
     }()
     
@@ -99,6 +100,11 @@ class LoginController: UIViewController {
             viewModel.password = sender.text
         }
         updateForm()
+    }
+    
+    @objc func handleShowResetPassword() {
+        let controller = ResetPasswordController()
+        navigationController?.pushViewController(controller, animated: true)
     }
     
     // MARK: - Helpers
